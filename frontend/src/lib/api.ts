2a00060a2a619fbecde41b4d7ad5ef73513c32e6
Francis-250 <munyankindif0@gmail.com>
2026-05-08 11:02:24 +0200

    change_api_url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL =
-  import.meta.env.VITE_APP_BASE_URL || "http://localhost:4000/api";
+export const API_BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
